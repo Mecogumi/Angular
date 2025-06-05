924226b07ece829cff7216f79952eda3c7f3100e
Jose Damian Gomez Hernandez <jose.gomez@correo.uady.mx>
2025-06-05 11:36:50 -0600

    Class 01

---
+++ typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,10 @@
+
+
+const name: string = 'John Doe';
+let hpPoints: number | 'Full' = 100;
+const isAlive: boolean = true;
+
+hpPoints = "Full";
+console.log({ name, hpPoints, isAlive })
+
+export { };
