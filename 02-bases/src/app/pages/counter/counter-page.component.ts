6e1976e38e4a866dc4272e6d4cb04fc46b0d574f
Jose Damian Gomez Hernandez <jose.gomez@correo.uady.mx>
2025-06-06 13:28:26 -0600

    Class 03

--- 02-bases/src/app/pages/counter/counter-page.component.ts
+++ 02-bases/src/app/pages/counter/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal, Signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter-page.component.html',
@@ -7,14 +7,18 @@ import { Component } from "@angular/core";
 
 export class CounterPageComponent {
   counter: number = 10;
+  counterSingal = signal(10)
 
   increaseBy(value: number) {
     this.counter += value;
+    this.counterSingal.update((current) => current + value)
   }
   decreaseBy(value: number) {
     this.counter -= value;
+    this.counterSingal.update((current) => current - value)
   }
   reset() {
     this.counter = 0
+    this.counterSingal.set(0);
   }
 }
