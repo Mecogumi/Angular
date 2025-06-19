0c74a3ea7afb2fd108e267c27102d7a4a28fa107
Jose Damian Gomez Hernandez <jose.gomez@correo.uady.mx>
2025-06-19 14:57:18 -0600

    class 02

--- 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ 05-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, JsonPipe, SlicePipe, UpperCasePipe, KeyValuePipe, TitleCasePipe, AsyncPipe } from '@angular/common';
+import { interval, tap } from 'rxjs';
 
 const client1 = {
   name: 'Damian',
@@ -19,7 +20,7 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, UpperCasePipe, KeyValuePipe, TitleCasePipe, AsyncPipe],
   templateUrl: './uncommon-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -40,4 +41,46 @@ export default class UncommonPageComponent {
     this.client.set(client1)
   }
 
+  //I18nPluralPipe
+
+  clientsMap = signal({
+    '=0': 'no tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    other: 'tenemos # clientes esperando'
+  })
+
+  clients = signal([
+    'maria',
+    'pedro',
+    'fernando',
+    'damian',
+    'jose',
+    'melissa',
+    'mariana',
+    'diana',
+    'andrea'
+  ])
+
+  deleteClient() {
+    this.clients.update((current) => current.slice(1))
+  }
+
+  //keyValuePipe
+  profile = {
+    name: 'Damian',
+    age: 24,
+    address: 'Merida'
+  }
+
+  //AsyncPipe
+  promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa')
+      console.log('promesa finalizada')
+    }, 3500)
+  })
+
+  myObservableTimer = interval(2000).pipe(
+    tap(value => console.log(value))
+  )
 }
